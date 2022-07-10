3c663eec5402049727ddb17161e15a284aeabeda
김민철 <kmc901206@gmail.com>
2022-07-10 12:39:19 +0900

    4. Fragment의 동작 관련 코드 작성

--- app/src/main/java/com/neppplus/fragmentpractice_20220710/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragmentpractice_20220710/fragments/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.neppplus.fragmentpractice_20220710.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragmentpractice_20220710.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,6 +18,14 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        동작 관련 코드 작성
+        toastBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "첫 프래그먼트 입니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
